2c16df5876031fdcdd9da2ce5e4a994bf0b200d1
Aaron Melendez <aaron.melendez37@gmail.com>
2025-04-30 23:49:01 -0700

    first paint

--- src/game.ts
+++ src/game.ts
@@ -1,9 +1,17 @@
 const canvas = document.getElementById('gameCanvas') as HTMLCanvasElement;
 // gives us methods to draw within our 2d canvas element
-const ctx = canvas.getContext('2d');
+const ctx = canvas.getContext('2d') as CanvasRenderingContext2D;
 
-const CANVAS_WIDTH = canvas.width = 600;
-const CANVAS_HEIGHT = canvas.height = 600;
+const CANVAS_WIDTH = canvas.width = window.innerWidth;
+const CANVAS_HEIGHT = canvas.height = window.innerHeight;
 
 const playerImage = new Image(); // need to get basic sprite images
-// playerImage.src = // path to sprite sheet
+playerImage.src = './assets/skinny-knight.png';
+
+function animate() {
+    ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    ctx.drawImage(playerImage, 0, 0);
+    requestAnimationFrame(animate);
+}
+
+animate();
