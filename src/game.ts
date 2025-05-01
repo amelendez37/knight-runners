64189ffa6946f7c8fb2041e812d8bbf2d22e7643
Aaron Melendez <aaron.melendez37@gmail.com>
2025-05-01 00:48:18 -0700

    exploring movement

--- src/game.ts
+++ src/game.ts
@@ -1,3 +1,5 @@
+import { checkIsInBounds } from "./utils";
+
 const canvas = document.getElementById('gameCanvas') as HTMLCanvasElement;
 // gives us methods to draw within our 2d canvas element
 const ctx = canvas.getContext('2d') as CanvasRenderingContext2D;
@@ -5,13 +7,38 @@ const ctx = canvas.getContext('2d') as CanvasRenderingContext2D;
 const CANVAS_WIDTH = canvas.width = window.innerWidth;
 const CANVAS_HEIGHT = canvas.height = window.innerHeight;
 
-const playerImage = new Image(); // need to get basic sprite images
-playerImage.src = './assets/skinny-knight.png';
+const playerImage = new Image();
+playerImage.src = './assets/min-knight-128.png'; // todo: load sprite sheet
 
-function animate() {
-    ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-    ctx.drawImage(playerImage, 0, 0);
-    requestAnimationFrame(animate);
+const playerState = {
+    xPos: 0,
+    yPos: 0,
+};
+
+function setupPlayers() {
+    document.addEventListener('keydown', (e) => {
+        // if (!checkIsInBounds()) return;
+        // don't do it this way. track key presses instead and update character speed
+        switch (e.key) {
+            case 'a':
+                playerState.xPos--;
+                break;
+            case 'd':
+                playerState.xPos++;
+                break;
+        }
+    });
 }
 
-animate();
+function gameLoop() {
+    ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    ctx.drawImage(playerImage, playerState.xPos, playerState.yPos);
+    requestAnimationFrame(gameLoop);
+}
+
+function run() {
+    setupPlayers();
+    gameLoop();
+}
+
+run();
---
+++ src/utils/index.ts
@@ -0,0 +1,3 @@
+export function checkIsInBounds() {
+    return true;
+}
