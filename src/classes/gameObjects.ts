58f12b7678269ab5d73ba8f1ea7ad8c263406f90
Aaron Melendez <aaron.melendez37@gmail.com>
2025-06-08 11:58:14 -0700

    lava class

--- src/classes/gameObjects.ts
+++ src/classes/gameObjects.ts
@@ -338,3 +338,14 @@ export class Platform extends BaseObject {
     }
   }
 }
+
+export class Lava extends BaseObject {
+  constructor(
+    x: number,
+    y: number,
+    ctx: CanvasRenderingContext2D,
+    gameState: GameState
+  ) {
+    super(x, y, ctx, gameState);
+  }
+}
